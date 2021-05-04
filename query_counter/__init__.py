ada2ff418fd43928b032a588de3d8c230e348fa4
Oleg Smedyuk <conformist.mw@gmail.com>
2021-05-04 22:33:14 +0300

    fix errors during install package

--- query_counter/__init__.py
+++ query_counter/__init__.py
@@ -1,6 +1,3 @@
-from .query_counter import DjangoQueryCounterMiddleware  # noqa: F401
-from .query_counter import queries_counter  # noqa: F401
-
 __version__ = '0.1.0'
 
 default_app_config = 'query_counter.apps.DjangoQueryCounterConfig'
