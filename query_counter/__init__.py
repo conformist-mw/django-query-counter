77684ff147702ccdd42e65427910d84c9e3d4ad0
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2022-07-09 16:32:20 +0300

    suppress default_app_config warning

--- query_counter/__init__.py
+++ query_counter/__init__.py
@@ -1,3 +1,6 @@
+import django
+
 __version__ = '0.2.0'
 
-default_app_config = 'query_counter.apps.DjangoQueryCounterConfig'
+if django.VERSION < (3, 2):
+    default_app_config = 'query_counter.apps.DjangoQueryCounterConfig'
