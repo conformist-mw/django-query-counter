9f1bb54fc10ae19fc648f147d59121bc6984931a
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2023-06-13 11:01:00 +0300

    try to import django, fix while install

--- query_counter/__init__.py
+++ query_counter/__init__.py
@@ -1,6 +1,8 @@
-import django
-
 __version__ = '0.2.2'
 
-if django.VERSION < (3, 2):
-    default_app_config = 'query_counter.apps.DjangoQueryCounterConfig'
+try:
+    import django
+    if django.VERSION < (3, 2):
+        default_app_config = 'query_counter.apps.DjangoQueryCounterConfig'
+except ImportError:
+    ...
