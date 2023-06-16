2071cba8d086581dc24aad187bf58eb6cb403c4c
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2023-06-16 10:31:00 +0300

    finish test project prepearing for testing

--- tests/settings.py
+++ tests/settings.py
@@ -1,18 +1,11 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-SECRET_KEY = "key"
 DEBUG = True
-
-INSTALLED_APPS = ["app"]
-
-MIDDLEWARE = []
-
-ROOT_URLCONF = "urls"
-
+USE_TZ = False
+SECRET_KEY = 'key'
+INSTALLED_APPS = ['apps']
+ROOT_URLCONF = 'urls'
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
     },
 }
